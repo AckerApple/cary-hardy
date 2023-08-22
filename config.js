eeda8dc67d92227ce1c5b9e0b69159d42c4fb211
Acker Apple <y>
2023-08-22 10:50:13 -0400

    update

--- config.js
+++ config.js
@@ -1,3 +1,7 @@
-import packageFile from "./config.json" assert { type: "json" }
+// Not supported in FireFox
+// import packageFile from "./config.json" assert { type: "json" }
+
+// This would be a json file but lack of support to import a json file
+import packageFile from "./config.json.js"
 
 export default packageFile
--- config.json.js
+++ config.json.js
@@ -1,3 +1,3 @@
-{
+export default {
   "nextMeetupDate": "August 21, 2023 21:00:00 EDT"
 }
