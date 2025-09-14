cdd3af8728d0ae25c86b9b259c4a1453937ba1f1
Acker Apple <acker@bstock.com>
2025-09-14 17:36:54 -0400

    update

--- src/home.tag.ts
+++ src/home.tag.ts
@@ -270,7 +270,7 @@ export const homeTag = tag(() => (
             onmouseover=${(e) => e.target.style.transform = 'scale(1.1)'}
             onmouseout=${(e) => e.target.style.transform = 'scale(1)'}
           >
-            Wrong Kong! shirt is up!
+            Wrong Kong shirt is up!
           </a>
         </div>
         <div style="display: flex; flex-direction: column; align-items: center; gap: 1em;">
