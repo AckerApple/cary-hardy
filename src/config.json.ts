614f9f352264e157291977371444f59410fc26d1
Acker Apple <acker@bstock.com>
2025-10-22 15:43:13 -0400

    update

--- src/config.json.ts
+++ src/config.json.ts
@@ -1,5 +1,5 @@
 // generate next meeting by going to https://caryhardy.com/admin.html
 // then run `npm run deploy`
 export default {
-  "nextMeetupDate": 1759194000000
+  "nextMeetupDate": 1761613200000
 }
