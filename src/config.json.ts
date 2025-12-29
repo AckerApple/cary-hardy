cfcca6ff0e208559fdf210fa6d751ce6bff6460e
Acker Apple <y>
2025-12-29 08:31:40 -0500

    update

--- src/config.json.ts
+++ src/config.json.ts
@@ -1,5 +1,5 @@
 // generate next meeting by going to https://caryhardy.com/admin.html
 // then run `npm run deploy`
 export default {
-  "nextMeetupDate": 1764036000000
+  "nextMeetupDate": 1767060000000
 }
